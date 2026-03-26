82d694c4619663f1fe4f40443d13def7c8a93dd9
Shahdidul <theshahidkhan.2004@gmail.com>
2026-03-26 23:30:24 +0600

    refactor: reduced complexity

--- second_max_min.cpp
+++ second_max_min.cpp
@@ -6,8 +6,6 @@
 using namespace std;
 
 pair<pair<int, int>, pair<int, int>> max_min (vector<int> &arr, int beginning, int end);
-int secondMax3(int first, int second, int third);
-int secondMin3(int first, int second, int third);
 
 int main () {
     vector<int> arr = {-1, 1, 3, 4, 2, -5, 14, 3, 22, -22, 4, 5, 33, -53};
@@ -22,24 +20,8 @@ pair<pair<int, int>, pair<int, int>> max_min (vector<int> &arr, int beginning, i
     pair <pair<int, int>, pair<int, int>> res_left = max_min(arr, beginning, mid);
     pair <pair<int, int>, pair<int, int>> res_right = max_min(arr, mid + 1, end);
     int maximum = max(res_left.first.first, res_right.first.first);
-    int secondMaximum = secondMax3(res_left.first.first, res_right.first.first, max(res_left.second.first, res_right.second.first));
+    int secondMaximum = max(min(res_left.first.first, res_right.first.first), max(res_left.second.first, res_right.second.first));
     int minimum = min(res_left.first.second, res_right.first.second);
-    int secondMinimum = secondMin3(res_left.first.second, res_right.first.second, min(res_left.second.second, res_right.second.second));
+    int secondMinimum = min(max(res_left.first.second, res_right.first.second), min(res_left.second.second, res_right.second.second));
     return {{maximum, minimum}, {secondMaximum, secondMinimum}};
-}
-
-int secondMax3(int first, int second, int third) {
-    int max = (first > second) ? first : second;
-    int secondMax = (first < second) ? first : second;
-    max = (max > third) ? max : third;
-    secondMax = (secondMax < third && third < max) ? third : secondMax;
-    return secondMax;
-}
-
-int secondMin3(int first, int second, int third) {
-    int min = (first < second) ? first : second;
-    int secondMin = (first > second) ? first : second;
-    min = (min < third) ? min : third;
-    secondMin = (third < secondMin && third > min) ? third : secondMin;
-    return secondMin;
 }
