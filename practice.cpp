fe01df353750c883021ff8a88b2346fc2c64a41f
Shahdidul <theshahidkhan.2004@gmail.com>
2026-05-01 20:05:03 +0600

    chore: practiced counting vowels using dnc

--- practice.cpp
+++ practice.cpp
@@ -3,17 +3,24 @@
 
 using namespace std;
 
-int countEven(vector<int> &arr, int s, int e);
+int countVowels (string st, int s, int e);
+bool isVowel(char c);
 
 int main() {
-    vector<int> arr = {1, 2, 3, 4, 5, 4, 5, 6, 8, 9, 22};
-    cout << countEven(arr, 0, arr.size() - 1) << endl;
+    string st = "Apple, banana, beef and chicken is good";
+    cout << countVowels(st, 0, st.length() - 1) << endl;
 }
 
-int countEven(vector<int> &arr, int s, int e) {
-    if(s == e) return !(arr[s] % 2);
+int countVowels (string st, int s, int e) {
+    if (s == e) return isVowel(st[s]);
     int mid = (s + e) / 2;
-    int l = countEven(arr, s, mid);
-    int r = countEven(arr, mid + 1, e);
+    int l = countVowels(st, s, mid);
+    int r = countVowels(st, mid + 1, e);
     return l + r;
+}
+
+bool isVowel(char c) {
+    c = tolower(c);
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') return true;
+    else return false;
 }
