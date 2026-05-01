e13de815ed3b68d0843f7e8caec7e0ffce0e9962
Shahdidul <theshahidkhan.2004@gmail.com>
2026-05-01 19:00:12 +0600

    chore: practiced max min using DnC

--- practice.cpp
+++ practice.cpp
@@ -1,49 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <utility>
 
 using namespace std;
 
-void merge_sort(vector<int> &arr, int s, int e);
-void merge(vector<int> &arr, int s, int mid, int e);
+pair<int, int> max_min(vector<int> &arr, int s, int e);
 
 int main() {
-    vector<int> arr = {14, 15, -1, 4, 10, 2};
-    merge_sort(arr, 0, arr.size() - 1);
-    for (int i = 0; i < arr.size(); i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+    vector<int> arr = {-1, 1, 3, 4, 2, -5, 14, 3, 22, -22, 4, 5, 33};
+    pair<int, int> m = max_min(arr, 0, arr.size() - 1);
+    cout << "max: " << m.first << " min: " << m.second << endl;
 }
 
-void merge_sort(vector<int> &arr, int s, int e) {
-    if (s == e) return;
+pair<int, int> max_min(vector<int> &arr, int s, int e) {
+    if (s == e) return {arr[s], arr[s]};
     int mid = (s + e) / 2;
-    merge_sort(arr, s, mid);
-    merge_sort(arr, mid+1, e);
-    merge(arr, s, mid, e);
-}
-
-void merge(vector<int> &arr, int s, int mid, int e) {
-    int i = s;
-    int j = mid + 1;
-    vector<int> mArr;
-    while (i <= mid && j <= e) {
-        if (arr[i] <= arr[j]) {
-            mArr.push_back(arr[i]);
-            i++;
-        }
-        else {
-            mArr.push_back(arr[j]);
-            j++;
-        }
-    }
-    while (i <= mid) {
-        mArr.push_back(arr[i]);
-        i++;
-    }
-    while (j <= e) {
-        mArr.push_back(arr[j]);
-        j++;
-    }
-    for(int i = s, j = 0; i <= e; i++, j++) arr[i] = mArr[j];
+    pair<int, int> l = max_min(arr, s, mid);
+    pair<int, int> r = max_min(arr, mid + 1, e);
+    int maximum = max(l.first, r.first);
+    int minimum = min(l.second, r.second);
+    return {maximum, minimum};
 }
