85441a3205da541b6a99d054b9fb5e5f3e3546b7
Shahdidul <theshahidkhan.2004@gmail.com>
2026-05-01 21:27:19 +0600

    chore: practiced majority

--- practice.cpp
+++ practice.cpp
@@ -1,48 +1,27 @@
 #include <iostream>
 #include <vector>
-#include <climits>
 
 using namespace std;
 
-class extreme {
-    public:
-    int max;
-    int max2;
-    int min;
-    int min2;
-};
-
-extreme maxmin(vector<int> &arr, int s, int e);
+int majority(vector<int> &arr, int s, int e);
 
 int main() {
-    vector<int> arr = {-1, 1, 3, 4, 2, -5, 14, 3, 22, -22, 4, 5, 33, -53};
-    extreme m = maxmin(arr, 0, arr.size() - 1);
-    cout << m.max << " " << m.max2 << endl;
-    cout << m.min << " " << m.min2 << endl;
+    vector<int> arr = {2, 3, 1, 3, 3, 3, 2};
+    int element = majority(arr, 0, arr.size() - 1);
+    cout << element << endl;
 }
 
-extreme maxmin(vector<int> &arr, int s, int e) {
-    if (s == e) return {arr[s], INT_MIN, arr[s], INT_MAX};
+int majority(vector<int> &arr, int s, int e) {
+    if (s == e) return arr[s];
     int mid = (s + e) / 2;
-    extreme l = maxmin(arr, s, mid);
-    extreme r = maxmin(arr, mid + 1, e);
-    extreme res;
-    if (l.max > r.max) {
-        res.max = l.max;
-        (r.max > l.max2) ? res.max2 = r.max : res.max2 = l.max2;
+    int l = majority(arr, s, mid);
+    int r = majority(arr, mid + 1, e);
+    if (l == r) return l;
+    int lcount = 0; int rcount = 0;
+    for(int i = s; i <= e; i++) {
+        if (arr[i] == l) lcount++;
+        else if (arr[i] == r) rcount++;
     }
-    else {
-        res.max = r.max;
-        (l.max > r.max2) ? res.max2 = l.max : res.max2 = r.max2;
-    }
-
-    if (l.min < r.min) {
-        res.min = l.min;
-        (l.min2 < r.min) ? res.min2 = l.min2 : res.min2 = r.min;
-    }
-    else {
-        res.min = r.min;
-        (r.min2 < l.min) ? res.min2 = r.min2 : res.min2 = l.min;
-    }
-    return res;
+    if (lcount > rcount) return l;
+    else return r;
 }
