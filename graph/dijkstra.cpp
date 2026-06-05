314414d981a1051eec05b84e3bebc6136d2baa15
Shahdidul <theshahidkhan.2004@gmail.com>
2026-06-05 09:28:32 +0600

    feat: implement Dijkstra's algorithm for finding shortest paths in a graph

---
+++ graph/dijkstra.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int main() {
+    int V, E;
+    cin >> V >> E;
+    vector<vector<pair<int, int>>> adjList(V);
+    for (int i = 0; i < E; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adjList[u].push_back({w, v});
+        adjList[v].push_back({w, u});
+    }
+    int src;
+    cin >> src;
+
+    // algo
+    vector<int> visited(V, 0);
+    vector<int> dist(V, INT_MAX);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    dist[src] = 0;
+    pq.push({dist[src], src});
+    while(!pq.empty()) {
+        pair<int, int> node = pq.top();
+        int u = node.second;
+        visited[u] = 1;
+        pq.pop();
+        for(auto adj: adjList[u]) {
+            int w = adj.first;
+            int v = adj.second;
+            if(!visited[v] && dist[v] > dist[u] + w) {
+                dist[v] = dist[u] + w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+
+    // print distance
+    for (int i = 0; i < V; i++)
+    {
+        if (dist[i] == INT_MAX)
+        {
+            cout << "src to " << i << ": inf" << endl;
+        }
+        cout << "src to " << i << ": " << dist[i] << endl;
+    }
+}
