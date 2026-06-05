92b531135b2e489a4e6c7ed6eeabb3e352b13762
Shahdidul <theshahidkhan.2004@gmail.com>
2026-06-05 18:07:15 +0600

    feat: implement Bellman-Ford algorithm for finding shortest paths in a graph

---
+++ graph/bellmen-ford.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int main() {
+    int V, E;
+    cin >> V >> E;
+    vector<vector<int>> edges;
+    for(int i = 0; i < E; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        edges.push_back({u, v, w});
+    }
+    int src;
+    cin >> src;
+
+    vector<int> dist(V, INT_MAX / 2);
+    dist[src] = 0;
+    for(int i = 1; i <= V; i++) {
+        for(auto edge: edges) {
+            int u = edge[0], v = edge[1], w = edge[2];
+            if(dist[u] + w < dist[v]) {
+                if(i == V) {
+                    cout << "negative cycle found" << endl;
+                    break;
+                }
+                dist[v] = dist[u] + w;
+            }
+        }
+    }
+    for (int i = 0; i < V; i++)
+        cout << src << " to " << i << ": " << dist[i] << endl;
+}
